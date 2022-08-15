71e1b052ff7fdb26f529a4d9ba3de4dbf2fe2e56
Gerard C. Saperas <gcsaperas@konodrac.com>
2022-08-15 21:36:00 +0200

    # import `Login`, redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
 import Widgets from "./components/Widgets/Widgets";
+import { useSelector } from "react-redux";
+import { selectUser } from "./reducers/userSlice";
+import Login from "./views/Login/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <div className="app">
       {/* Header */}
